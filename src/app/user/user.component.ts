183845ae275468ee510244da57942efc54d5c4c5
Andrey Stypin <andrew.stypin@gmail.com>
2024-08-16 20:11:02 -0700

    2_41_ListContent (#3)
    * udate to for loop
    
    * if selected User use if statement
    
    * css and html task added
    
    * tasks added
    
    * task task
    
    * outsource interfaces User and Task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
