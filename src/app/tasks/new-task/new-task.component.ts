36b92b6619a336fcb99cb0a2a072b5072cc71fb6
webrankie <andrew.stypin@gmail.com>
2024-08-18 14:42:23 -0700

    new task

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { type Task } from '../task/task.model';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {}
