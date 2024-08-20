85e338ffd344f20a68711968702402820fd7bd71
Andrey Stypin <andrew.stypin@gmail.com>
2024-08-19 20:24:46 -0700

    2_48_Styling (#4)
    * selected user use active color
    
    * completeTask
    
    * new task
    
    * cancel event
    
    * ng model two ways
    
    * add new task
    
    * ng-content use
    
    * date pipe use
    
    * add service for task to add task
    
    * remove task with service

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,35 @@
+import { Component, Output, EventEmitter, inject, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
+import { TaskService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+  private taskService = inject(TaskService);
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.taskService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        dueDate: this.enteredDueDate,
+      },
+      this.userId
+    );
+    this.close.emit();
+  }
+}
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
+import { TaskService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
+  constructor(private TaskService: TaskService) {}
+
   @Input({ required: true }) task!: Task;
+
+  onCompleteTask() {
+    this.TaskService.removeTask(this.task.id);
+  }
 }
